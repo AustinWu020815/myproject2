549378c4a225d89261b122e5ddcddf2f3aa9bd9c
AustinWu020815 <wu020707@gmail.com>
2022-03-04 16:12:18 +0800

    initial commit

---
+++ src/main/java/com/austin/ScoringS.java
@@ -0,0 +1,10 @@
+package com.austin;
+
+public class ScoringS {
+    public static void main(String[] args) {
+        Student derek = new Student();
+        derek.name = "Derek";
+        derek.english = 100;
+        derek.math = 55;
+    }
+}
---
+++ src/main/java/com/austin/Student.java
@@ -0,0 +1,7 @@
+package com.austin;
+
+public class Student {
+        String name;
+        int english;
+        int math;
+}
