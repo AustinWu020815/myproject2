cafb30bb7353f8df5dcfa1cc55785d1248190edf
AustinWu020815 <wu020707@gmail.com>
2022-03-04 15:48:58 +0800

    initial commit

--- src/main/java/com/austin/Scoring.java
+++ src/main/java/com/austin/Scoring.java
@@ -3,7 +3,7 @@ package com.austin;
 public class Scoring {
     public static void main(String[] args) {
         String[] names = {"Andy", "Lisa", "Emily", "Scott"};
-        int[] mend = {85, 47, 98, 64};
+        int[] mand = {85, 47, 98, 64};
         int[] eng = {63, 99, 84, 55};
         int[] math = {96, 72, 51, 77};
         for (int i = 0; i < 4; i++) {
@@ -11,8 +11,8 @@ public class Scoring {
                 System.out.println(names[i]);
             }else {
                 int as;
-                as = (mend[i] + eng[i] + eng[i])/3;
-                System.out.println(names[i] + "\t" + mend[i] + "\t" + eng[i] + "\t" + math[i] + "\t" + as);
+                as = (mand[i] + eng[i] + eng[i])/3;
+                System.out.println(names[i] + "\t" + mand[i] + "\t" + eng[i] + "\t" + math[i] + "\t" + as);
             }
         }
     }
