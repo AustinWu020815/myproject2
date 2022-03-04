fd839a7f331250ae3b9f36b4f5af503b99539fff
AustinWu020815 <wu020707@gmail.com>
2022-03-04 15:32:26 +0800

    initial commit

---
+++ src/main/java/com/austin/scoring.java
@@ -0,0 +1,17 @@
+package com.austin;
+
+public class scoring {
+    public static void main(String[] args) {
+        String[] names = {"Andy", "Lisa", "Emily", "Scott"};
+        int[] mend = {85, 47, 98, 64};
+        int[] eng = {23, 99, 84, 55};
+        int[] math = {96, 72, 51, 77};
+        for (int i = 0; i < 4; i++) {
+            if (i == 2){
+                System.out.println(names[i]);
+            }else {
+                System.out.println(names[i] + "\t" + mend[i] + "\t" + eng[i] + "\t" + math[i]);
+            }
+        }
+    }
+}
