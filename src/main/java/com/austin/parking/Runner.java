63bf1cfbcaeb8386d05f0f6f9285849c1d3518de
Austin Wu <wu020707@gmail.com>
2022-05-13 16:22:22 +0800

    parking #2 fee test1

--- src/main/java/com/austin/parking/Runner.java
+++ src/main/java/com/austin/parking/Runner.java
@@ -7,7 +7,7 @@ public class Runner {
     public static void main(String[] args) {
         String id = "NAU-0859";
         String enter = "09:00";
-        String exit = "18:00";
+        String exit = "18:16";
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         Date t1 = null;
         try {
@@ -28,7 +28,8 @@ public class Runner {
             System.out.println("You have stay: " + mins + " mins");
 //          30 NTD per hour
             int fee = 30*(mins/60);
-            if ((int)((t2.getTime()-t1.getTime()) % (1000*60)) > 15){
+//          9:14 => 270 dollars ; 9:15 => 300 dollars
+            if ((mins % 60) > 14){
                 fee = 30*((mins/60) + 1);
             }
             System.out.println("Yuor fee is: " + fee + " dollars");
