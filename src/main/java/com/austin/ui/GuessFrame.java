0bf8f521d7d6ee5273161a1027cc37870d277979
Austin Wu <wu020707@gmail.com>
2022-05-27 14:33:59 +0800

    commit

---
+++ src/main/java/com/austin/ui/GuessFrame.java
@@ -0,0 +1,13 @@
+package com.austin.ui;
+
+import javax.swing.*;
+
+public class GuessFrame extends JFrame {
+    public GuessFrame(){
+        super();
+        setSize(300,200);
+        setLocation(650,450);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+}
