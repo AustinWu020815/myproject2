aff2b1df82c3204848ff82632e793464976e1018
Austin Wu <wu020707@gmail.com>
2022-05-27 15:13:52 +0800

    commit

--- src/main/java/com/austin/ui/GuessFrame.java
+++ src/main/java/com/austin/ui/GuessFrame.java
@@ -1,6 +1,9 @@
 package com.austin.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuessFrame extends JFrame {
     public GuessFrame(){
@@ -8,6 +11,22 @@ public class GuessFrame extends JFrame {
         setSize(300,200);
         setLocation(650,450);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        JButton button  = new JButton("Hallo!");
+        JLabel label = new JLabel("WoW");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+               // System.out.println("Hi");
+                label.setText("Hi");
+            }
+        });
+        setLayout(new FlowLayout());
+        add(button);
+        add(label);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        GuessFrame guessFrame = new GuessFrame();
     }
 }
