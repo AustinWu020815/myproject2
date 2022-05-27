7ca3c546996b1bc69df9b02ba63c6677dec810d5
Austin Wu <wu020707@gmail.com>
2022-05-27 14:03:29 +0800

    GUI commit

---
+++ src/main/java/com/austin/ui/MyForm.java
@@ -0,0 +1,18 @@
+package com.austin.ui;
+
+import javax.swing.*;
+
+public class MyForm {
+    private JButton helloButton;
+    private JPanel panel1;
+    private JButton clickHereButton;
+    private JButton helpButton;
+    private JTextField 歡迎光臨二埒日式料理TextField;
+    private JCheckBox 酥炸卡滋豬排飯CheckBox;
+    private JCheckBox KIRAKIRA丼CheckBox;
+    private JCheckBox 日式咖哩蛋包飯CheckBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
