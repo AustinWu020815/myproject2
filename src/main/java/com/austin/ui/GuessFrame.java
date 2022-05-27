e3eb357d54f2fb8ec17bd8632daa72069986892a
Austin Wu <wu020707@gmail.com>
2022-05-27 16:12:45 +0800

    guess number

--- src/main/java/com/austin/ui/GuessFrame.java
+++ src/main/java/com/austin/ui/GuessFrame.java
@@ -6,21 +6,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GuessFrame extends JFrame {
+    JButton button  = new JButton("Hallo!");
+    JLabel label = new JLabel("WoW");
+    JTextField number = new JTextField(7);
     public GuessFrame(){
         super();
         setSize(300,200);
         setLocation(650,450);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button  = new JButton("Hallo!");
-        JLabel label = new JLabel("WoW");
+
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                int num = Integer.parseInt(number.getText());
+                System.out.println(number);
                // System.out.println("Hi");
-                label.setText("Hi");
+               // label.setText("Hi");
             }
         });
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setVisible(true);
